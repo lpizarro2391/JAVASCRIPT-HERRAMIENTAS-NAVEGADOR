ab555b23c3d99ec5cfb6bd91cbd22914c5fe54c5
lpizarro2391 <lourdespizarro@gmail.com>
2020-01-14 20:47:27 -0600

    consola

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-document.write('texto desde Java script')
+function escribirEnConsola(){
+  console.log('Hola desde la consola javascript')
+}
+
+setInterval(escribirEnConsola,1000)
+
+var vble1 = 'Texto de Prueba'
+var vble2 = 10
